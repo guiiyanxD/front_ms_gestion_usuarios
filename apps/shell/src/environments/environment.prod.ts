c259730986018b8259dadea9f5597246a1d4e731
Will Victus <williamswaz18@gmail.com>
2026-06-02 22:37:11 -0400

    testing reverse proxy

--- apps/shell/src/environments/environment.prod.ts
+++ apps/shell/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiGatewayUrl: 'http://uhqu8rqq4t33xe1c0012wvrb.155.133.27.31.sslip.io',
+  apiGatewayUrl: 'https://reverse-proxy-for-gestion-workspace.onrender.com',
 };
