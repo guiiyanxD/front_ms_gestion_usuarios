fd0f65f9cbaebe717b1f378e0f41e49a2b9eb962
Will Victus <williamswaz18@gmail.com>
2026-06-07 18:11:16 -0400

    testing con https

--- apps/shell/src/environments/environment.prod.ts
+++ apps/shell/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  graphqlUrl: 'https://reverse-proxy-for-gestion-workspace.onrender.com/graphql',
+  graphqlUrl: 'https://viralshopbo.com/graphiql',
 };
